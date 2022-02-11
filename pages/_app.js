978818e5de1608dd0fd1fc01ad4137731b7aa12f
Francogalfre <galfre.vn@gmail.com>
2022-02-11 13:36:35 -0300

    Framer motion fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,28 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <div className="bg-black">
       <Navbar />
-      <motion.div
-        key={router.route}
-        initial="pageInitial"
-        animate="pageAnimate"
-        variants={{
-          pageInitial: {
-            opacity: 0,
-          },
-          pageAnimate: {
-            opacity: 1,
-          },
-        }}
-      >
-        <Component {...pageProps} />
-      </motion.div>
+      <AnimatePresence>
+        <motion.div
+          key={router.route}
+          initial="pageInitial"
+          animate="pageAnimate"
+          variants={{
+            pageInitial: {
+              opacity: 0,
+            },
+            pageAnimate: {
+              opacity: 1,
+            },
+          }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
